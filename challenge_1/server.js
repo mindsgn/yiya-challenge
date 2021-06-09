5dab8102ee958812a5a97427c5a08c92be5c1fa8
mindsgn <seni.tembe@protonmail.com>
2021-06-09 02:12:13 +0200

    updated program

--- challenge_1/server.js
+++ challenge_1/server.js
@@ -3,27 +3,25 @@ const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 });;
-const API = 'ac7df731'
-// search parameters used to query API
-let id = null; // movie id
-let title = null; // movie title
-let type = null; // Type of result to return. movie, series, episode
-let year = null; // Type of result to return. movie, series, episode
-let plot = null; // Type of result to return. movie, series, episode
-
-let link = '';
+const API = 'ac7df731';
 
 const getTitle = () => {
     try{
         readline.question(`Hi, what movie would you like to search for? `, (input) => {
             if(input){
-                search(input);
+                if(input==="exit();"){
+                    console.log("thank you for using our service, Good bye.")
+                    process.exit()
+                }else{
+                    search(input);
+                }
             }else{
                 getTitle();
             }
        });
     }catch(error){
         console.log(error);
+        getTitle();
     }
 };
 
@@ -35,34 +33,38 @@ const getTags = () => {
         }
     }catch(error){
         console.log(error);
+        getTitle();
     }
 };
 
 const search = async (input) => {
     try{
         const link = `http://www.omdbapi.com/?&apikey=${API}&t=${input}&r=json`;
-
-        let data = null;
-        
+        let data = ``;
         await https.get(link, (response) => {
             // called when a data chunk is received.
             response.on('data', (chunk) => {
                 data += chunk;
             });
-
             // called when the complete response is received.
             response.on('end', () => {
                 if(data){
-                    console.log((data))
+                    console.clear();
+                    console.log("================================================================================")
+                    console.log("SEARCH RESULTS FOUND FOR: ", input)
+                    console.log("================================================================================")
+                    console.log(JSON.parse(data))
+                    console.log("\n")
+                    console.log("\n")
                     getTitle();
                 }
             });
-
         }).on("error", (error) => {
             console.log("Error: " + error.message);
         });
     }catch(error){
         console.log(error);
+        getTitle();
     }
 };
 
